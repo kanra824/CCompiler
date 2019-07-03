6d8f62ea9be274822e5aeaac4288474e7c162def
yohei <maeda.yohei.74m@st.kyoto-u.ac.jp>
2019-07-03 21:15:34 +0900

    Add operator of mul, div and parenthesis

--- src/mdcc.c
+++ src/mdcc.c
@@ -22,8 +22,51 @@ struct Token {
     char *str; // token string
 };
 
-// The token we are looking at
-Token *token;
+// kind of AST node
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 整数
+} NodeKind;
+
+// type of AST Node
+typedef struct Node Node;
+struct Node {
+    NodeKind kind; // type of node
+    Node *lhs; // left child
+    Node *rhs; // right child
+    int val; // use if kind == ND_NUM
+};
+
+//---------------------------------------------------------------
+// Function prototype
+
+// Error function
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+
+// Lexer
+bool consume(char op);
+void expect(char op);
+int expect_number();
+bool at_eof();
+Token *new_token(TokenKind kind, Token *cur, char *str);
+Token *tokenize(char *p);
+
+// Parser
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+Node *expr();
+Node *mul();
+Node *term();
+
+// Code generation
+void gen(Node *node);
+
+//---------------------------------------------------------------
+// Error function
 
 // report error
 void error(char *fmt, ...) {
@@ -34,9 +77,8 @@ void error(char *fmt, ...) {
     exit(1);
 }
 
-// input program
+// program input
 char *user_input;
-
 // report error and where it is occured
 void error_at(char *loc, char *fmt, ...) {
     va_list ap;
@@ -52,7 +94,14 @@ void error_at(char *loc, char *fmt, ...) {
 }
 
 
-/* if next token is expected symbol
+// -------------------------------------------------
+// Lexer
+
+
+// The token we are looking at
+Token *token;
+
+/* if next token is expected to be a symbol
  * then read next token and return true
  * else return false
  */
@@ -110,10 +159,13 @@ Token *tokenize(char *p) {
             continue;
         }
 
-        if(*p == '+' || *p == '-') {
+        if(*p == '+' || *p == '-' || *p == '*' || *p == '/' ||
+           *p == '(' || *p == ')') {
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
+
+        
 
         if(isdigit(*p)) {
             cur = new_token(TK_NUM, cur, p);
@@ -128,6 +180,106 @@ Token *tokenize(char *p) {
     return head.next;
 }
 
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Parser
+
+/*
+ * expr = mul ("+" mul | "-" mul)*
+ * mul = term ("*" term | "/" term)*
+ * term = num | "(" expr ")"
+ */
+
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr() {
+    Node *node = mul();
+    for(;;) {
+        if(consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+        } else if(consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *mul() {
+    Node *node = term();
+    for(;;) {
+        if(consume('*')) {
+            node = new_node(ND_MUL, node, term());
+        } else if(consume('/')) {
+            node = new_node(ND_DIV, node, term());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *term() {
+    // if next_token == '(' then term must be '(' expr ')'
+    if(consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+    // else term muse be a number
+    return new_node_num(expect_number());
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Code generation
+
+
+void gen(Node *node) {
+    if(node->kind == ND_NUM) {
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+
+    switch(node->kind) {
+        case ND_ADD:
+            printf("    add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("    sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("    imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("    cqo\n");
+            printf("    idiv rdi\n");
+    }
+
+    printf("    push rax\n");
+}
+
+//-------------------------------------------------
+// main
+
 int main(int argc, char **argv) {
     if(argc != 2) {
         error("引数の個数が正しくありません");
@@ -135,30 +287,17 @@ int main(int argc, char **argv) {
     }
 
     user_input = argv[1];
+    token = tokenize(user_input);
+    Node *node = expr();
 
-    // トークナイズする
-    token = tokenize(argv[1]);
-
-    // アセンブリの前半部分を出力
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    // 式の最初は数でなければいけない
-    printf("    mov rax, %d\n", expect_number());
+    // generate code
+    gen(node);
 
-    // '+ <数>' あるいは '- <数>' というトークンの並びを消費しつつ
-    // アセンブリを出力
-    while(!at_eof()) {
-        if(consume('+')) {
-            printf("    add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("    sub rax, %d\n", expect_number());
-    }
-
+    printf("    pop rax\n");
     printf("    ret\n");
     return 0;
 }
